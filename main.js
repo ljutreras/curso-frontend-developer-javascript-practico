41a865318242f7f915647e72bf2e72a8dde16189
Leonardo Utreras <ljutreras96@gmail.com>
2022-08-08 19:15:30 -0400

    Actializacion 22

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector(".desktop-menu");
 const menuHamIcon = document.querySelector(".menu");
 const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
 const mobileMenu = document.querySelector(".mobile-menu");
-const aside = document.querySelector(".product-detail");
+const shoppingCartContainer = document.querySelector("#shoppingCartContainer");
 const cardsContainer = document.querySelector(".cards-container");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
@@ -11,20 +11,20 @@ menuHamIcon.addEventListener("click", toogleMobileMenu);
 menuCarritoIcon.addEventListener("click", toogleCarritoAside);
 
 function toggleDesktopMenu() {
-  const isCarritoAsideOpen = aside.classList.contains("inactive");
+  const isCarritoAsideOpen = shoppingCartContainer.classList.contains("inactive");
 
   if (!isCarritoAsideOpen) {
-    aside.classList.add("inactive");
+    shoppingCartContainer.classList.add("inactive");
   }
 
   desktopMenu.classList.toggle("inactive");
 }
 
 function toogleMobileMenu() {
-  const isCarritoAsideOpen = aside.classList.contains("inactive");
+  const isCarritoAsideOpen = shoppingCartContainer.classList.contains("inactive");
 
   if (!isCarritoAsideOpen) {
-    aside.classList.add("inactive");
+    shoppingCartContainer.classList.add("inactive");
   }
 
   mobileMenu.classList.toggle("inactive");
@@ -37,7 +37,7 @@ function toogleCarritoAside() {
     mobileMenu.classList.add("inactive");
   }
 
-  aside.classList.toggle("inactive");
+  shoppingCartContainer.classList.toggle("inactive");
 }
 
 const productList = [];
